04169d73eb9e4da2aa1c923324ec4c29e8d244fc
Rafael Scherer <rafaelscherer3@gmail.com>
2026-06-25 20:44:45 -0300

    atualizacao 2

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -3,18 +3,28 @@
 //import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 //import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   //const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
